14993eb3e0412c8c9917fd9c4b8f0b58ce2a8e73
yadawvishalpc-cpu <yadawvishalpc@gmail.com>
2026-06-27 10:21:04 +0000

    container config  done

--- 01_basic/01_variable.js
+++ 01_basic/01_variable.js
@@ -1,15 +1,25 @@
+//Const = Const means fix value they not change 
+// Let = let means we change the value
+//
+
+
 const accountId = 144553
-let accountEmail = "hitesh@google.com"
+let accountEmail = "Vishal@google.com"
 var accountPassword = "12345"
 accountCity = "Jaipur"
 let accountState;
 
-// accountId = 2 // not allowed
+// accountId = 2 // not allowed  
+// because  that acc id is const that not change 
+// ACC ID change nhi hoga agr ek baar likh diya to wahi se change hoga kaha pehle likha hai eg. 1 No 
 
 
-accountEmail = "hc@hc.com"
+accountEmail = "yv@yv.com"
 accountPassword = "21212121"
 accountCity = "Bengaluru"
+//Email pass city 
+// we are change any time 
+
 
 console.log(accountId);
 
@@ -20,3 +30,6 @@ because of issue in block scope and functional scope
 
 
 console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
+
+
+// console.table use hotya ki sari cheeze ek sath print ho jaye or ek hi baar me 
