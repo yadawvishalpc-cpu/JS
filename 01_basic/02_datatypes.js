a0af8807844cb77368e80c278554e2206ff25efd
yadawvishalpc-cpu <yadawvishalpc@gmail.com>
2026-06-27 11:37:27 +0000

    Datatypes

---
+++ 01_basic/02_datatypes.js
@@ -0,0 +1,28 @@
+let name = "Vishal"
+let age = 18
+let isLoggedIn = true
+
+
+
+
+//number = 
+
+//string = use in doble comma like this ""
+// exampole "vishal"
+
+//boolean = True / false   
+//   (Means ki ye sirf ha OR n me use hota hai  )
+
+//bigint = use for larger no 
+
+// null = standalone value  Means NUll is a type 
+//(null is empy means khali ) null is object 
+
+
+// undefined = vvalue is not defined 
+
+// symbols = use for defined unquie 
+
+// object = 
+
+
