417a9bad7f622589a1bd5be9b64305cd43c6cc55
yadawvishalpc-cpu <yadawvishalpc@gmail.com>
2026-06-28 09:56:01 +0000

    Operations discussed

---
+++ 01_basic/03_conversionOpreation.js
@@ -0,0 +1,27 @@
+let score = "33abc"
+//Agr hum value change karege to output bhi change hoga mins abh "33"abc hai to iska out put 
+// output -- undefined
+//string
+//number
+//NaN
+// YEH HAI OTPUT 
+
+// OR NULL USE HOGA TO OUT 0 HOGA
+// BOOLEAM USE HOGA LIKE TRUE TO OUTPUT 1 AAYEGA OF FALSE TO 0
+//UNDEFINE KA OUTPUT NAN HOGA
+
+
+
+console.log(typeof scrore);
+console.log(typeof(score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+
+//"33" NUM CHANGE ESAILY 
+// "33ABC"IS TYPE TO USE KRTE HAUI TO CHANGE NHI HOGA OUTPUT NAN AAYEGA
+// TRUE 1 OR FALSE 0
+
+let isloggedIn = 1 
+let booleanIsLoggedIn = boolean(isLoggedIn)
+console.log(booleanIsLoggrdIn)
