29155b25e3ca0c1a747a067ae5f3cc4736c2a30f
yadawvishalpc-cpu <yadawvishalpc@gmail.com>
2026-06-29 07:32:56 +0000

    compare

---
+++ 01_basic/04_comparision.js
@@ -0,0 +1,19 @@
+console.log(2>1)
+console.log(2<1)
+console.log(2==2)
+// output come in boolean means true or false 
+
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+// they both created problem 
+//means jo ye upr or niche code likha ye ye bhot jyada confusion type ka hai 
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+// === triple equally  check strickly  
+
+
+console.log("2" === 2);
